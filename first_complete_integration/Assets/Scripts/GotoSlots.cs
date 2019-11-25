b366502e46ee96f2d1de83d93b38338588bf6bda
oozwal <ujjawalpra@gmail.com>
2019-11-25 12:40:43 +0545

    scripts_added

---
+++ first_complete_integration/Assets/Scripts/GotoSlots.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class GotoSlots : MonoBehaviour, IDropHandler
+{
+    public int my_index;
+
+    public void Start()
+    {
+        
+    }
+    public GameObject item
+    {
+        get
+        {
+            if(transform.childCount>0)
+            {
+                return transform.GetChild(0).gameObject;
+            }
+            return null;
+        }
+
+        
+    }
+    public void OnDrop(PointerEventData eventData)
+    {
+        if(!item)
+        {
+            ActualIndex item_index = ImageDragger.item_being_dragged.transform.GetChild(0).GetComponent<ActualIndex>();
+            my_index = numberPositionHandler.num_handler_instance.returnMyIndex(this.gameObject);
+
+            if (item_index.actual_index == my_index)
+            {
+             
+                ImageDragger.item_being_dragged.transform.SetParent(transform);
+                numberPositionHandler.num_completed += 1;
+
+            }
+        }
+
+    }
+}
---
+++ first_complete_integration/Assets/Scripts/ImageDragger.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ImageDragger : MonoBehaviour,IDragHandler,IBeginDragHandler,IEndDragHandler
+{
+
+    public static GameObject item_being_dragged;
+    Transform my_parent;
+    Vector2 my_pos;
+    Image my_image;
+    Color temp_color;
+    float alpha_val = 0.5f;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        item_being_dragged = this.gameObject;
+        my_pos = this.transform.position;
+        my_parent = transform.parent;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        temp_color.a = alpha_val;
+        my_image.color = temp_color;
+        transform.position = eventData.position;
+        this.GetComponent<CanvasGroup>().blocksRaycasts = false;
+        if(transform.parent==my_parent)
+        {
+
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        item_being_dragged = null;
+        temp_color.a = 1;
+        my_image.color = temp_color;
+        if(transform.parent == my_parent)
+        {
+            transform.position = my_pos;
+        }
+        else
+        {
+            transform.position = transform.parent.position;
+        }
+        this.GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        my_image = this.GetComponent<Image>();
+        temp_color = my_image.color;
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
